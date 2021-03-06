f708102a6198ebdeb42f13cc79074cb767b80976
Fernando Leis <fleis.wa@rd.com.br>
2021-03-06 11:12:58 -0300

    chassi do projeto criado

---
+++ src/test/java/br/com/psaccesslog/PsAccessLogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.psaccesslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsAccessLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
